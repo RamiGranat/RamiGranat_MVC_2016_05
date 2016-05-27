0e32b180f9512879884f61aea6fc7f7bcf247ddf
RamiGranat <raextheassassin@gmail.com>
2016-05-27 08:58:46 +0200

    Add project files.

---
+++ RamiGranat_MVC_2016_05/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RamiGranat_MVC_2016_05.Startup))]
+namespace RamiGranat_MVC_2016_05
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
